c0df9c259ef1dcbfe62c194261a56aee19d1b88d
doshikevin361 <doshikevin361@gmail.com>
2025-12-05 18:03:59 +0530

    ok

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,7 @@ const _geist = Geist({ subsets: ['latin'] });
 const _geistMono = Geist_Mono({ subsets: ['latin'] });
 
 
+
 export const metadata: Metadata = {
   title: 'LuxeLoom - Exquisite Jewelry Collection',
   description: 'Discover our complete collection of exquisite jewelry. Handcrafted pieces for every occasion.',
