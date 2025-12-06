302d0e1435de30ff7e7656326feafbdfc1cbd352
Shivang Nstack <shivangprajapati504@gmail.com>
2025-12-06 14:48:59 +0530

    fixed search

--- components/home/SearchBar/SearchBar.tsx
+++ components/home/SearchBar/SearchBar.tsx
@@ -17,17 +17,18 @@ const SearchBar = () => {
 
   return (
     <form onSubmit={handleSearch} className='w-full'>
-      <div className='flex items-center rounded-full border border-[#E6D3C2] bg-white px-4 py-2 text-sm shadow-sm'>
+      <div className='flex items-center w-full rounded-full border border-[#E6D3C2] bg-white pl-4 pr-3 py-2 text-sm shadow-sm'>
         <input
           type='text'
           value={query}
           onChange={event => setQuery(event.target.value)}
           placeholder='Search for jewelry, materials, or collections'
-          className='ml-2 flex-1 bg-transparent text-[#1F3B29] placeholder:text-[#B7A28F] focus:outline-none'
+          className='flex-1 bg-transparent text-[#1F3B29] placeholder:text-[#B7A28F] focus:outline-none'
         />
+
         <button
           type='submit'
-          className='rounded-full bg-[#1F3B29] p-2 text-sm font-semibold text-white transition hover:bg-[#16301F]'>
+          className='flex items-center justify-center rounded-full bg-[#1F3B29] w-9 h-9 text-white transition hover:bg-[#16301F]'>
           <Search size={16} />
         </button>
       </div>
@@ -36,5 +37,3 @@ const SearchBar = () => {
 };
 
 export default SearchBar;
-
-
