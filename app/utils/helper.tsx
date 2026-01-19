e752420b9558789cd2de3724751f13728b7e48f0
Shivang Nstack <shivangprajapati504@gmail.com>
2026-01-19 10:55:11 +0530

    f

--- app/utils/helper.tsx
+++ app/utils/helper.tsx
@@ -1,14 +1,18 @@
-export const formatIndianDate = (dateString:string) => {
-  if (!dateString) return "";
+export const formatIndianDate = (dateString: string) => {
+  if (!dateString) return '';
 
   const date = new Date(dateString);
 
-  return date.toLocaleString("en-IN", {
-    day: "2-digit",
-    month: "short",
-    year: "numeric",
-    hour: "2-digit",
-    minute: "2-digit",
-    hour12: true,
-  }).replace(",", "");
+  return date
+    .toLocaleString('en-IN', {
+      day: '2-digit',
+      month: 'short',
+      year: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+      hour12: true,
+    })
+    .replace(',', '');
 };
+
+export const data = () => {};
